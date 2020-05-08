24deaf866aecffcd20e709c4f3b342bc5064dfb1
themockingjedi <themockingjedi@users.noreply.github.com>
2020-05-08 21:26:28 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -11,9 +11,9 @@ int main(void)
     while (n < 1 || n > 8);
     for (int r = 0; r < n; r++) //row
     { 
-        for(int d = 0; d < -r + (n - 1); d++) //DOTS
+        for(int d = 0; d <= -r + (n - 1); d++) //DOTS
            {
-               printf("");
+               printf(" ");
            }
                 for (int c = 0; c <= r; c++) //column
                 {
